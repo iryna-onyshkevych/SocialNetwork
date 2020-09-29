890bb4ce0e5ae48e1ea3552c348218d5afd877f3
Iryna Onyshkevych <onyshkevych.iryna@gmail.com>
2020-09-29 08:37:03 +0300

    Class User was added

--- SocialNetwork/MainWindow.xaml.cs
+++ SocialNetwork/MainWindow.xaml.cs
@@ -21,26 +21,31 @@ namespace SocialNetwork
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    class Post
+    class User
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        [BsonElement("title")]
-        public string Title { get; set; }
-        [BsonElement("comments")]
-        public List<Comment> Comments { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("surname")]
+        public string Surname { get; set; }
+        [BsonElement("email")]
+        public string Email { get; set; }
+        [BsonElement("interests")]
+        public string[] Interests { get; set; }
 
     }
-    class Comment
-    {
-        [BsonElement("body")]
-        public string CommentBody { get; set; }
-        [BsonElement("users")]
-        public string User { get; set; }
-        [BsonElement("date")]
-        public object CommentCreateDate { get; set; }
-    }
+    //class Interests
+    //{
+    //    [BsonElement("body")]
+    //    public string CommentBody { get; set; }
+    //    [BsonElement("users")]
+    //    public string User { get; set; }
+    //    [BsonElement("date")]
+    //    public object CommentCreateDate { get; set; }
+    //}
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -48,13 +53,34 @@ namespace SocialNetwork
             InitializeComponent();
             var client = new MongoClient("mongodb://localhost:27017");
             var database = client.GetDatabase("socialnetwork");
-            IMongoCollection<BsonDocument> postBsonColection = database.GetCollection<BsonDocument>("users");
-            var filterBuilder = Builders<BsonDocument>.Filter;
-            var filter = filterBuilder.Eq("age", 45);
-            var postBSON = postBsonColection.Find(filter).ToList();
-            int b = postBSON.Count();
-            MessageBox.Show(b.ToString());
-            var newPost = new Post();
+            IMongoCollection<BsonDocument> postsBsonCollection = database.GetCollection<BsonDocument>("users");
+            //var filterBuilder = Builders<BsonDocument>.Filter;
+            //var filter = filterBuilder.Eq("age", 45);
+            //var postBSON = postsBsonCollection.Find(filter).ToList();
+            //var posts = postsBsonCollection.Find(filter).Project("{title:1").ToList();
+            //int b = postBSON.Count();
+            //MessageBox.Show(b.ToString());
+            var newPost = new User();
+            //newPost.Name = String.Format("Oksana", DateTime.Now.ToString());
+            //newPost.Interests = new string[] { "bowling" };
+            IMongoCollection<User> postsCollection = database.GetCollection<User>("users");
+            //postsCollection.InsertOne(newPost);
+            newPost.Name = ("Olena");
+            postsCollection.ReplaceOne(p => p.Id == newPost.Id, newPost);
+            postsCollection.DeleteOne(p => p.Id == newPost.Id);
+            var courses = database.GetCollection<User>("users");
+            //var course = courses.FindAllAs<users>().SetFields(Fields.Include("Title", "Description").Exclude("_id")).ToList(); 
+
+            //posts1.Text += course;
+
+            //List<User> list = postsCollection.AsQueryable().ToList<User>();
+            //var posts = from p in list select p;
+            //foreach (User p in posts)
+            //{
+            //    Console.WriteLine(p.Name);
+            //}
+
         }
+        
     }
 }
