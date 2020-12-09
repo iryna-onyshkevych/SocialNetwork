2d949cad90eb9c0ee50d454c30f34978ce61d021
Iryna Onyshkevych <onyshkevych.iryna@gmail.com>
2020-12-09 17:59:37 +0200

    DAL added

---
+++ DAL/Enteties/Postcomments.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Enteties
+{
+    public class Postcomments
+    {
+        [BsonIgnoreIfDefault]
+        public ObjectId CommentOwnerId { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string Text { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string Date { get; set; }
+    }
+}
