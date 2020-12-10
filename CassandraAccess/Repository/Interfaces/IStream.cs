019eb3d9194b39637693ed528f6d36f5c26ced6d
Iryna Onyshkevych <onyshkevych.iryna@gmail.com>
2020-12-10 15:30:12 +0200

    IStream.cs added

--- CassandraAccess/Repository/Interfaces/IStream.cs
+++ CassandraAccess/Repository/Interfaces/IStream.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cassandra;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace CassandraAccess.Repository.Interfaces
 {
     class IStream
     {
+        void SyncUserStream(ISession session, Guid postId, TimeUuid updatedAtPrev);
+
     }
 }
