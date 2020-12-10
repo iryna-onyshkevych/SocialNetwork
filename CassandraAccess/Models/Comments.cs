d07addef8213755fd4b6da157a08c3bd52cd58f9
Iryna Onyshkevych <onyshkevych.iryna@gmail.com>
2020-12-10 14:48:10 +0200

    CassandraAccess was added

---
+++ CassandraAccess/Models/Comments.cs
@@ -0,0 +1,16 @@
+﻿using Cassandra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessCassandra.Models
+{
+    public class Comments
+    {
+        public TimeUuid CommentId { get; set; }
+        public string Comment { get; set; }
+        public Guid Commentator { get; set; }
+    }
+}
