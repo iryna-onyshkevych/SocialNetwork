db54218e8d72b816e9297a307d7dbd152740d516
Iryna Onyshkevych <onyshkevych.iryna@gmail.com>
2020-12-10 14:51:18 +0200

    Stream was added

--- CassandraAccess/Models/Stream.cs
+++ CassandraAccess/Models/Stream.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Cassandra;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CassandraAccess.Models
+namespace DataAccessCassandra.Models
 {
-    class Stream
+    public class Stream
     {
+        public string UserName { get; set; }
+        public TimeUuid LastUpdatedAt { get; set; }
+        public Guid PostId { get; set; }
+        public string AuthorId { get; set; }
+        public string Content { get; set; }
+
+        private List<Comments> Comments { get; set; }
     }
 }
