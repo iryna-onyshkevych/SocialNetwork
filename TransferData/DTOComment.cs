45299e4a96fecaf39d95ff169b978f15fee7b199
Iryna Onyshkevych <onyshkevych.iryna@gmail.com>
2020-12-10 15:38:07 +0200

    TransferData added

---
+++ TransferData/DTOComment.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferData.Models
+{
+    public class DTOComment
+    {
+        [BsonElement("CommentId")]
+        public int Comment_Id { get; set; }
+
+        [BsonElement("AuthorId")]
+        public int Author_Id { get; set; }
+        [BsonElement("CommentText")]
+        public string Comment_Text { get; set; }
+       
+
+        [BsonElement("CreateDate")]
+        public BsonTimestamp Create { get; set; }
+
+        [BsonElement("ModifyDate")]
+        public BsonTimestamp Modify { get; set; }
+    }
+}
---
+++ TransferData/DTOUser.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace DataTransfer.Models
+{
+    public class DTOUser
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("UserId")]
+        public int UserId { get; set; }
+
+        [BsonElement("UserLogin")]
+        public string UserLogin { get; set; }
+
+        [BsonElement("UserPassword")]
+        public string UserPassword { get; set; }
+
+        [BsonElement("UserName")]
+        public string UserName { get; set; }
+
+        [BsonElement("Email")]
+        public string Email { get; set; }
+
+        [BsonElement("Interests")]
+        public List<string> Interests { get; set; }
+        [BsonElement("FriendsId")]
+        public List<int> FriendsId { get; set; }
+
+        [BsonElement("RegisterDate")]
+        public BsonTimestamp RegisterDate { get; set; }
+    }
+}
